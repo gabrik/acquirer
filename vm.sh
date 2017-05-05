7a71d9d6ee8b74f001d5b3ed7b79d246dcc49f2a
Gabriele Baldoni <gabriele.baldoni@gmail.com>
2017-05-05 12:35:44 +0200

    fixed script for running under mininet testdeb

--- vm.sh
+++ vm.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-kvm -name vm1 -m 256 -net bridge,br=br0,name=br0 -net nic,model=virtio,netdev=mynet0 -netdev bridge,id=mynet0 -kernel acquirer.bin -append  '{  "rc": [{"bin" : "hello.bin",,"argv" : ["192.168.34.1",,"50040"],,}],,"net":{"if":"vioif0",,"type":"inet",,"method":"dhcp"}},,'
-
-
-
+kvm -name vm1 -m 256 -net bridge,br=s1-br0,name=s1-br0 -net nic,model=virtio,macaddr=DE:AD:BE:EF:00:AA,netdev=mynet0 -netdev bridge,id=mynet0,br=s1-br0 -kernel acquirer.bin -append  '{  "rc": [{"bin" : "hello.bin",,"argv" : ["192.168.2.2",,"50040"],,}],,"net":{"if":"vioif0",,"type":"inet",,"method":"dhcp"}},,'
